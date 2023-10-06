b091da1ff6d792b0b458e5e584d3c5986c5f17ac
thesafatem <thesafatem@gmail.com>
2023-10-07 03:33:28 +0600

    Add string representation for suit and value

--- suit.h
+++ suit.h
@@ -7,3 +7,22 @@ enum class Suit {
     Hearts,
     Diamonds
 };
+
+std::string toString(Suit suit) {
+    switch (suit) {
+        case Suit::Spades:
+            return "♠";
+            break;
+        case Suit::Clubs:
+            return "♣";
+            break;
+        case Suit::Hearts:
+            return "♡";
+            break;
+        case Suit::Diamonds:
+            return "♢";
+            break;
+        default:
+            return "X";
+    }
+}
--- value.h
+++ value.h
@@ -16,3 +16,49 @@ enum class Value {
     King = 13,
     Ace = 14
 };
+
+std::string toString(Value value) {
+    switch (value) {
+        case Value::Two:
+            return "2";
+            break;
+        case Value::Three:
+            return "3";
+            break;
+        case Value::Four:
+            return "4";
+            break;
+        case Value::Five:
+            return "5";
+            break;
+        case Value::Six:
+            return "6";
+            break;
+        case Value::Seven:
+            return "7";
+            break;
+        case Value::Eight:
+            return "8";
+            break;
+        case Value::Nine:
+            return "9";
+            break;
+        case Value::Ten:
+            return "10";
+            break;
+        case Value::Jack:
+            return "J";
+            break;
+        case Value::Queen:
+            return "Q";
+            break;
+        case Value::King:
+            return "K";
+            break;
+        case Value::Ace:
+            return "A";
+            break;
+        default:
+            return "X";
+    }
+}
