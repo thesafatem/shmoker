096ad78d7e962c980d6d07a30a9b075e1de4bbd2
thesafatem <thesafatem@gmail.com>
2023-10-07 04:32:23 +0600

    Overload compare operators for BestHand

--- handRankings.cpp
+++ handRankings.cpp
@@ -14,6 +14,55 @@ BestHand::operator std::string() const {
     return out.str();
 }
 
+bool BestHand::operator<(BestHand const& rhs) const {
+    if (this->handRanking != rhs.handRanking) {
+        return this->handRanking < rhs.handRanking;
+    }
+    for (
+        auto it1 = this->hand.begin(), it2 = rhs.hand.begin(); 
+        it1 != this->hand.end() && it2 != rhs.hand.end(); 
+        it1++, it2++
+    ) {
+        if (*it1 < *it2) return true;
+        if (*it1 > *it2) return false;
+    }
+    return false;
+}
+
+bool BestHand::operator>(BestHand const& rhs) const {
+    if (this->handRanking != rhs.handRanking) {
+        return this->handRanking > rhs.handRanking;
+    }
+    for (
+        auto it1 = this->hand.begin(), it2 = rhs.hand.begin(); 
+        it1 != this->hand.end() && it2 != rhs.hand.end(); 
+        it1++, it2++
+    ) {
+        if (*it1 > *it2) return true;
+        if (*it1 < *it2) return false;
+    }
+    return false;
+}
+
+bool BestHand::operator==(BestHand const& rhs) const {
+    if (this->handRanking != rhs.handRanking) {
+        return false;
+    }
+    for (
+        auto it1 = this->hand.begin(), it2 = rhs.hand.begin(); 
+        it1 != this->hand.end() && it2 != rhs.hand.end(); 
+        it1++, it2++
+    ) {
+        if (*it1 > *it2) return false;
+        if (*it1 < *it2) return false;
+    }
+    return true;
+}
+
+bool BestHand::operator!=(BestHand const& rhs) const {
+    return !(*this == rhs);
+}
+
 std::ostream & operator <<(std::ostream &out, const BestHand &bh) {
     out << toString(bh.handRanking) << "\n";
     for (auto card : bh.hand) {
@@ -41,6 +90,8 @@ BestHand HandRankings::getBestHand(Cards hand) {
         HandRankings::isOnePair,
         HandRankings::isHighCard
     };
+
+    HandRankings::sort(hand);
     
     for (auto rf : rankingFunctions) {
         BestHand bh = rf(hand);
--- handRankings.h
+++ handRankings.h
@@ -62,6 +62,10 @@ public:
     std::vector<Card> hand;
     BestHand(HandRanking handRanking, std::vector<Card> hand);
 
+    bool operator<(BestHand const& rhs) const;
+    bool operator>(BestHand const& rhs) const;
+    bool operator==(BestHand const& rhs) const;
+    bool operator!=(BestHand const& rhs) const;
     operator std::string() const;
     friend std::ostream & operator <<(std::ostream &out, const BestHand &bh);
 };
